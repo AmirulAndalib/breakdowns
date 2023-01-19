0baf5b3a278fd5d0a6f47f44ce8c7fb951b7d110
Hafitz Setya <breakdownsproject@gmail.com>
2023-01-19 16:35:32 +0700

    Update activity_updater.py

--- activity_updater.py
+++ activity_updater.py
@@ -259,7 +259,7 @@ if __name__ == "__main__":
     readme = root / "README.md"
     readme_contents = readme.open().read()
     # List Activity
-    data = fetch_activity(TOKEN, userId=6088410)
+    data = fetch_activity(TOKEN, userId=161753)
     res = "\n".join(
         [
             "* [{status} {progress}]({activity_url}) of [{title}]({url})".format(**x)
